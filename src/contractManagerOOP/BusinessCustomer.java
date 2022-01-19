e789c5db2494f4e5e31a7fedb7d7ee5c9deb3589
Duncan R Skilton <dr.skilton@mbro.ac.uk>
2022-01-19 13:06:49 +0000

    updated BusinessCustomer with Override methods

--- src/contractManagerOOP/BusinessCustomer.java
+++ src/contractManagerOOP/BusinessCustomer.java
@@ -9,6 +9,80 @@ package contractManagerOOP;
  *
  * @author MC03353
  */
-public class BusinessCustomer {
+public class BusinessCustomer implements CustomerInterface {
+    
+    public BusinessCustomer(){
+        
+    }
+    
+    @Override
+    public String getFirstName() {
+        return "";
+    }
+
+    @Override
+    public void setFirstName(String name) {
+
+    }
+
+    @Override
+    public String getLastName() {
+        return "";
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+
+    }
+
+    @Override
+    public String getPackageType() {
+        return "";
+    }
+
+    @Override
+    public void setPackageType(String packageType) {
+
+    }
+
+    @Override
+    public String getDataBundle() {
+        return "";
+    }
+
+    @Override
+    public void setDataBundle(String dataBundle) {
+
+    }
+
+    @Override
+    public String getReference() {
+        return "";
+    }
+
+    @Override
+    public void setReference(String reference) {
+
+    }
+
+    @Override
+    public int getContractLength() {
+        return -1;
+    }
+
+    @Override
+    public void setContractLength(int contractLengthMonths) {
+
+    }
+
+    @Override
+    public boolean getInternationalCallsIncluded() {
+        return true;
+    }
+
+    @Override
+    public void setInternationalCallsIncluded(boolean internationalCallsIncluded) {
+
+    }
     
 }
